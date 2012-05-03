4ca6150c3fe2e1226687461295309877ed087394
Gervel Giva <gervelgiva@gmail.com>
2012-05-03 09:58:27 -0400

    Set up the epdev app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epdev::Application.config.secret_token = '34694cc5171b0dd9ce36b75c8d2c44361e3cbff54d76ddac61ce54eed4e6c27c5e10d042d43ce2950b712c0f136a99ada5a6da5a3e43c3278aad5e0b6646482c'
